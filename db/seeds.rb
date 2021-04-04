9376142276a2d4f76afd0f22c61605f57f914a36
Igor Stein <igor_stein@yahoo.de>
2021-04-04 17:33:30 +0200

    add faker gem and user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "faker"
+
+User.destroy_all
+ComicBook.destroy_all
+Purchase.destroy_all
+
+10.times do
+  user = User.new(
+    username: Faker::Book.title,
+    email: Faker::Internet.email,
+    password: "123456"
+    )
+
+user.save
+puts "Creating #{user.username}"
+end
