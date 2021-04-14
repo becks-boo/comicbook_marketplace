3cfa48927d5cdda96b220b51cee5e4d6ea83f948
Igor Stein <igor_stein@yahoo.de>
2021-04-14 16:35:05 +0200

    add input for pictures in new and edit form and display pictures in show page

--- app/models/comic_book.rb
+++ app/models/comic_book.rb
@@ -2,6 +2,7 @@ class ComicBook < ApplicationRecord
   belongs_to :user
 
   has_many :purchases
+  has_many_attached :pictures, dependent: :destroy
 
   validates :name, :location, :quantity, :price, :description, presence: true, allow_blank: false
   validates :description, length: { minimum: 5, too_short: "Please type at least a 5 character long description", maximum: 500, too_long: "500 characters is the maximum allowed" }
