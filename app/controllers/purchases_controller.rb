1a84b94372bbbb7e3e2f61a6510e11f4d02f689d
Igor Stein <igor_stein@yahoo.de>
2021-04-18 17:41:42 +0200

    add very basic details to purchase show page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   def show
     @purchase = Purchase.find(params[:id])
+    @comic_book = ComicBook.find(@purchase.comic_book_id)
 
     authorize @purchase
   end
